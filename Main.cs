2659e7378c423a434d4444526cbf3be214684fda
Andrew Wilder <agwilder@gmail.com>
2024-12-04 17:47:19 -0800

    initial commit

--- Main.cs
+++ Main.cs
@@ -15,27 +15,6 @@ public partial class Main : Node
 		_sprite = GetNode<CharacterBody2D>("player");
 		_ball = GetNode<RigidBody2D>("ball");
 		_line = GetNode<Line2D>("trajLine");
-		
-		if (_sprite == null){
-			GD.Print("Sprite is null");			
-		} else {
-			GD.Print("Not null sprite");
-		}
-
-		if (_line == null){
-			GD.Print("Line is null");
-		} else {
-			GD.Print("Not null line");
-		}
-
-		if (_ball == null){
-			GD.Print("Ball is null");
-		} else {
-			GD.Print("Not null ball");
-		}
-
-	}
-
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
